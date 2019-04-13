c1acddb0aee17be5ef974f6d182f2912768b1dfb
raman@techvisionit.com <raman@techvisionit.com>
2019-04-13 16:39:34 +0530

    code

---
+++ src/main/resources/entity/example/Users.java
@@ -0,0 +1,26 @@
+package entity.example;
+
+public class Users {
+
+	private String name;
+	private String email;
+	private String password;
+	
+	public setName(String name) {
+		
+		this.name = name;
+	}
+	public String getName() { return this.name; } 
+	
+	public setEmail(String email) {
+		
+		this.email = email;
+	}
+	public String getEmail() { return this.email; } 
+	
+	public setpassword(String pwd) {
+		
+		this.password = pwd;
+	}
+	public String getpassword() { return this.password; } 
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+
+<p> 
+<h1> Test Applicaton </h1>
+
+
+</p>
+<p>
+
+	Name : <input type="text" name="name" properties="users" />
+</p>
+
+</body>
+</html>
